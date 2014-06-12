fc63ba28152e86853371d93c7f422fad45c397d4
Pavel Rebrov <pavel@rebrov.ru>
2014-06-12 18:20:38 +0800

    Add placeholder data frame for results

--- ProgrammingAssignment3/rankall.R
+++ ProgrammingAssignment3/rankall.R
@@ -1,6 +1,7 @@
 rankall <- function(outcome, num = "best") {
     ## Read outcome data
     data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    results <- data.frame()
     
     ## Check that outcome is valid
     if(!(outcome %in% c("heart attack", "heart failure", "pneumonia"))) stop("invalid outcome") ## check if outcome is one of the listed
